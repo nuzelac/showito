030a5ba91a99ad5a6a58e5bfbf3a0b84d774ec16
tost <glupimejl@gmail.com>
2014-12-02 21:15:55 +0100

    upgrade ruby, rails

--- prod_test.sh
+++ prod_test.sh
@@ -1,1 +1,1 @@
-curl -X GET http://www.showito.com/api/scrobbles/fix/new\?filename\=how.i.met.your.mother.s09e08.720p.hdtv.x264-2hd.mkv\&access_token\=546fab4a9d1c1e351c671612a4a74c59
+curl -4 -X GET http://www.showito.com/api/scrobbles/fix/new\?filename\=how.i.met.your.mother.s09e08.720p.hdtv.x264-2hd.mkv\&access_token\=546fab4a9d1c1e351c671612a4a74c59
